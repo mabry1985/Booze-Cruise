326b3a68541eb18bb4aeea81d8d1d3538bddeb09
Josh Mabry <mabry1985@gmail.com>
2019-09-07 21:41:58 -0700

    add logo and stylesheet for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../assets/images/booze-cruise-logo.png';
+import './header.scss';
 
 function Header() {
+
   return (
-    <nav>
-      <ul className="left">
-        <li>
-          <Link to="/">Beer List</Link>
-        </li>
-        <li>
-          <Link to="/add-keg">Add Keg</Link>
-        </li>
-      </ul>
-      <h1 className="title">Booze Cruise</h1>
+    <nav id='navbar'>
+      <img
+        className='site-logo'
+        alt='booze cruise logo'
+        src={logo} />
+        <span>
+          <h3>
+            <Link to="/">Beer List </Link>
+            |
+            <Link to="/add-keg"> Add Keg</Link>
+          </h3>
+        </span>
     </nav>
   );
 }
--- src/components/NewForm.jsx
+++ src/components/NewForm.jsx
@@ -4,19 +4,41 @@ function NewForm() {
   return (
     <div>
       <form>
+        <label
+          className='access-labels'
+          htmlFor='name'>
+          Name
+        </label>
         <input
           type='text'
-          id='names'
-          placeholder='Name'/>
+          name='name'
+          id='name'
+          placeholder='Name'
+          required />
         <br />
+        <label
+          className='access-labels'
+          htmlFor='brewery'>
+          Brewery
+        </label>
         <input
           type='text'
+          name='brewery'
           id='brewery'
-          placeholder='Brewery'/>
+          placeholder='Brewery'
+          required />
         <br />
+        <label
+          className='access-labels'
+          htmlFor='abv'>
+          Name
+        </label>
         <input
+          type='text'
+          name='abv'
           id='abv'
-          placeholder='ABV'/>
+          placeholder='ABV'
+          required />
         <br />
         <button type='submit'>Add Keg</button>
       </form>
