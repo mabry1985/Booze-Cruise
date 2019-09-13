3e40288247a41ec218dde101e38f322c7d39e2ff
Josh Mabry <mabry1985@gmail.com>
2019-09-13 12:24:10 -0700

    change newkegform to state component add state checkedRadio

--- src/components/NewKegForm.jsx
+++ src/components/NewKegForm.jsx
@@ -1,106 +1,121 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function NewKegForm(props) {
-  let _name = null;
-  let _brewery = null;
-  let _abv = null;
+class NewKegForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-  function handleNewKegFormSubmission(event) {
+    this.state = {
+      checkedRadio: 'micro',
+    };
+
+    let _name = null;
+    let _brewery = null;
+    let _abv = null;
+  };
+
+  checkPrice = () => {
+    let price = ()
+  }
+
+  handleNewKegFormSubmission = (event) => {
     event.preventDefault();
 
     props.onNewKegCreation({
-      name: _name.value,
-      brewery: _brewery.value,
-      abv: _abv.value,
+      name: this._name.value,
+      brewery: this._brewery.value,
+      abv: this._abv.value,
+      price: this.checkPrice,
     });
 
     _name.value = '';
     _brewery.value = '';
     _abv.value = '';
+  };
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.handleNewKegFormSubmission}>
+          <label
+            htmlFor='name'>
+            Name
+          </label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Name'
+            required
+            ref={(input) => {this._name = input;}} />
+
+          <br />
+          <label
+            htmlFor='brewery'>
+            Brewery
+          </label>
+          <input
+            type='text'
+            name='brewery'
+            id='brewery'
+            placeholder='Brewery'
+            required
+            ref={(input) => {this._brewery = input;}}/>
+
+          <br />
+          <label
+            htmlFor='abv'>
+            Name
+          </label>
+          <input
+            type='text'
+            name='abv'
+            id='abv'
+            placeholder='ABV'
+            required
+            ref={(input) => {this._abv = input;}}/>
+
+            <div className="beer-type">
+              <label>
+                <input
+                  type="radio"
+                  name="beer-price-radio"
+                  value="domestic"
+                  checked={this.state.checkedRadio = 'domestic'}
+                  className="form-check-input"
+                />
+                Domestic
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="beer-price-radio"
+                  value="micro"
+                  checked={this.state.checkedRadio = 'micro'}
+                  className="form-check-input"
+                />
+                Micro
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="beer-price-radio"
+                  value="import"
+                  checked={this.state.checkedRadio = 'import'}
+                  className="form-check-input"
+                />
+                Foreign
+              </label>
+            </div>
+
+          <br />
+          <button type='submit'>Add Keg</button>
+        </form>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <label
-          htmlFor='name'>
-          Name
-        </label>
-        <input
-          type='text'
-          name='name'
-          id='name'
-          placeholder='Name'
-          required
-          ref={(input) => {_name = input;}} />
-
-        <br />
-        <label
-          htmlFor='brewery'>
-          Brewery
-        </label>
-        <input
-          type='text'
-          name='brewery'
-          id='brewery'
-          placeholder='Brewery'
-          required
-          ref={(input) => {_brewery = input;}}/>
-
-        <br />
-        <label
-          htmlFor='abv'>
-          Name
-        </label>
-        <input
-          type='text'
-          name='abv'
-          id='abv'
-          placeholder='ABV'
-          required
-          ref={(input) => {_abv = input;}}/>
-
-          <div className="beer-type">
-            <label>
-              <input
-                type="radio"
-                name="beer-price-radio"
-                value="domestic"
-                checked={true}
-                className="form-check-input"
-              />
-              Domestic
-            </label>
-
-            <label>
-              <input
-                type="radio"
-                name="beer-price-radio"
-                value="micro"
-                checked={false}
-                className="form-check-input"
-              />
-              Micro
-            </label>
-
-            <label>
-              <input
-                type="radio"
-                name="beer-price-radio"
-                value="import"
-                checked={false}
-                className="form-check-input"
-              />
-              Foreign
-            </label>
-          </div>
-
-        <br />
-        <button type='submit'>Add Keg</button>
-      </form>
-    </div>
-  );
 }
 
 NewKegForm.propTypes = {
