0537bbc0f05ae4985342da67c8eaafa565955eba
Josh Mabry <mabry1985@gmail.com>
2019-09-15 11:04:27 -0700

    update keg to include symbols for abv and price in render

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 function Keg(props) {
   return (
     <div>
-      <h3>{props.name} - {props.abv}</h3>
+      <h3>{props.name} - {props.abv}%</h3>
       <p><em>{props.brewery}</em></p>
-      <p>{props.price}</p>
+      <p>${props.price}.00</p>
       <p>{props.pintsLeft}</p>
       <button
         onClick={() => props.onSellBeer(props.id)}>
